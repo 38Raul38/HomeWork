5b343826e7f1510031a78f0b675d43bad7429d23
38Raul38 <raulseidov2006@gmail.com>
2025-01-30 01:24:22 +0400

    SQL Lesson 1

--- ConsoleApp2/Interface/IUser.cs
+++
@@ -1,8 +0,0 @@
-namespace ConsoleApp2;
-public interface IUser
-{
-    public Guid Id { get; set; }
-    public Guid ShowroomId { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
--- lesson_9_10/Substraction.cs
+++
@@ -1,7 +0,0 @@
-namespace ConsoleApp1;
-
-public class Subtraction : ICalculatorOperation
-{
-    public string Name { get; set; } = nameof(Subtraction);
-    public double Execute(double a, double b) => a - b;
-}
