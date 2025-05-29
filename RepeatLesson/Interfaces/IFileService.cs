a506e157e8adaa978433c640521798e59cd93079
38Raul38 <raulseidov2006@gmail.com>
2025-05-29 21:50:53 +0400

    Upgrade File Service

---
+++ RepeatLesson/Interfaces/IFileService.cs
@@ -0,0 +1,15 @@
+namespace Lesson11.Interfaces;
+using Data.Model;
+
+public interface IFileService
+{
+    public void Save(Results result)
+    {
+        
+    }
+
+    public void Delete()
+    {
+        
+    }
+}
