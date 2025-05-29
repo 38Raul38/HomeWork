74133ea9b54cc3f3829625d57cb03e690c1d118b
38Raul38 <raulseidov2006@gmail.com>
2025-05-29 21:09:48 +0400

    correction File Service

--- JS_1/Task3/script.js
+++
@@ -1,5 +0,0 @@
-    document.body.onclick = function (event) {
-      let ball = document.querySelector('ball');
-      ball.style.top  = event.clientY + 'px';
-      ball.style.left = event.clientX + 'px';
-    };
