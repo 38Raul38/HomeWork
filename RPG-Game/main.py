ba777a1f1f3bd119c4ccb53f96de0ea288ca03e7
38Raul38 <raulseidov2006@gmail.com>
2026-04-17 01:27:10 +0400

    RPG Game

--- RPG-Game/main.py
+++ RPG-Game/main.py
@@ -1,5 +1,5 @@
-import random   
-                                                 ############____ Character ____############
+import random
+
 class Character:
     def __init__(self, name, health, attack_power):
         self.name = name
@@ -11,10 +11,6 @@ class Character:
         other.health -= damage
         print(f"{self.name} attacks {other.name} for {damage} damage!")
 
-    def damage(self, amount):
-        self.health -= amount
-        print(f"{self.name} takes {amount} damage! Remaining health: {self.health}")
-
     def character_info(self):
         print(f"Name: {self.name}, Health: {self.health}, Attack Power: {self.attack_power}")
 
@@ -23,79 +19,68 @@ class Character:
 
     def __add__(self, other):
         if isinstance(other, Character):
-            combined_name = f"{self.name} & {other.name}"
-            combined_health = self.health + other.health
-            combined_attack_power = self.attack_power + other.attack_power
-            return Character(combined_name, combined_health, combined_attack_power)
-        return NotImplemented    
+            return Character(
+                f"{self.name} & {other.name}",
+                self.health + other.health,
+                self.attack_power + other.attack_power
+            )
+        return NotImplemented
 
     def __lt__(self, other):
         if isinstance(other, Character):
             return self.health < other.health
-        return NotImplemented   
+        return NotImplemented
 
     def __eq__(self, other):
         if isinstance(other, Character):
             return self.health == other.health and self.attack_power == other.attack_power
         return NotImplemented
-    
+
     def __len__(self):
         return int(self.attack_power)
 
     def __bool__(self):
         return self.health > 0
 
-                                                ############____ Warrior ____############
-class Warrior(Character):
-    def __init__(self, name, health, attack_power):
-        super().__init__(name, health, attack_power)
 
-    def Dismember(self, other):
+class Warrior(Character):
+    def attack(self, other):
         damage = 20
         self.health += 10
         other.health -= damage
-        print(f"{self.name} performs a special attack on {other.name} for {damage} damage!") 
+        print(f"{self.name} dismembers {other.name} for {damage} damage!")
 
-                                                ############____ Mage ____############
+
 class Mage(Character):
-    def __init__(self, name, health, attack_power):
-        super().__init__(name, health, attack_power)
-
-    def Finger_of_Death(self, other):
+    def attack(self, other):
         damage = 40
         other.health -= damage
         if other.health <= 0:
             self.attack_power += 3
-        print(f"{self.name} casts a fireball on {other.name} for {damage} damage!")
+        print(f"{self.name} casts Finger of Death on {other.name} for {damage} damage!")
 
-                                                ############____ Archer ____############
+
 class Archer(Character):
-    def __init__(self, name, health, attack_power):
-        super().__init__(name, health, attack_power)
-
-    def Marksmanship(self, other):
-        if random.random() < 0.35:
-            damage = 50
-        else:
-            damage = 30
-        
+    def attack(self, other):
+        damage = 50 if random.random() < 0.35 else 30
         other.health -= damage
-        print(f"{self.name} shoots an arrow at {other.name} for {damage} damage!")
+        print(f"{self.name} shoots {other.name} for {damage} damage!")
 
 
+characters = [
+    Mage("Leon", 100, 15),
+    Warrior("WK", 120, 10),
+    Archer("Robin", 90, 12)
+]
 
-
-
-
-
-leon =Mage("Leon", 100, 15)
-wk = Warrior("WK", 120, 10)
-
-leon.character_info()
-wk.character_info()
 print("--- Battle Start ---")
-leon.attack(wk)
-wk.Dismember(leon)
+
+for i in range(len(characters)):
+    attacker = characters[i]
+    target = characters[(i + 1) % len(characters)]
+    attacker.attack(target)
+
 print("--- Battle End ---")
-leon.character_info()
-wk.character_info()
+
+for c in characters:
+    c.character_info()
