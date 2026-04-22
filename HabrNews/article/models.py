109dad427f0e694e53636b803e72abd94546452d
38Raul38 <raulseidov2006@gmail.com>
2026-04-22 21:46:08 +0400

    Update

--- HabrNews/article/models.py
+++ HabrNews/article/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    author = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to='article_pictures/') #(лучше хранить на https://cloudinary.com/)
+    content = models.TextField()
+    likes = models.IntegerField(default=0)  #надо сделать так, чтобы пользователь мог поставить только один лайк или дизлайк, и не мог менять свое мнение после этого. Для этого нужно создать отдельную модель для хранения информации о том, кто поставил лайк или дизлайк.
+    dislikes = models.IntegerField(default=0) #надо сделать так, чтобы пользователь мог поставить только один лайк или дизлайк, и не мог менять свое мнение после этого. Для этого нужно создать отдельную модель для хранения информации о том, кто поставил лайк или дизлайк. 
+    title = models.CharField(max_length=200)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+        ordering = ['-published_date']
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=30, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
--- HabrNews/article/views.py
+++ HabrNews/article/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from article.models import Article
+
+def article_list(request):
+    # Здесь можно получить список статей из базы данных и передать их в шаблон
+    articles = Article.objects.all()
+    return render(request, 'article_list.html', {'articles': articles})
